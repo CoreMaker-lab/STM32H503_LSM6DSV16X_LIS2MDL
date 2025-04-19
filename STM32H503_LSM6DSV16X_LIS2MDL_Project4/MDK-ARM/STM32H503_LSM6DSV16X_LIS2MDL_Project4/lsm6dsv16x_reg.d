76236a9079159a37b2ec0d40a571674607ef081f
CoreMaker <a845656974@outlook.com>
2025-04-19 22:56:49 +0800

    update

---
+++ STM32H503_LSM6DSV16X_LIS2MDL_Project4/MDK-ARM/STM32H503_LSM6DSV16X_LIS2MDL_Project4/lsm6dsv16x_reg.d
@@ -0,0 +1,5 @@
+stm32h503_lsm6dsv16x_lis2mdl_project4/lsm6dsv16x_reg.o: \
+  ..\Core\Src\lsm6dsv16x_reg.c ..\Core\Inc\lsm6dsv16x_reg.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h
